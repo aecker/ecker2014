e55470c133c56031b29b14bf04b4d169c726618b
Alexander Ecker <aecker@tuebingen.mpg.de>
2012-03-16 12:39:06 +0100

    Couple of changes to SpikeCountSet

--- +nc/SpikeCountSet.m
+++ +nc/SpikeCountSet.m
@@ -24,31 +24,32 @@ classdef SpikeCountSet < dj.Relvar & dj.AutoPopulate
             insert(self, key);
             b = key.spike_count_start;
             e = key.spike_count_end;
-            [unitId, spikeTimes] = fetchn(ephys.Spikes(key), 'unit_id', 'spike_times');
-            [trialNum, startTime] = fetchn(stimulation.StimTrials(key) & 'valid_trial = true', 'trial_num', 'start_time');
-            nTrials = numel(trialNum);
-            for i = 1:numel(unitId)
-                fprintf('Unit %d\n', unitId(i))
-                t = spikeTimes{i};
+            for unitKey = fetch(ephys.Spikes(key))'
+                fprintf('Unit %d\n', unitKey.unit_id)
+                t = fetch1(ephys.Spikes(unitKey), 'spike_times');
                 nSpikes = numel(t);
                 k = 0;
-                spikeCount = zeros(nTrials, 1);
-                for j = 1:numel(trialNum)
-                    while k > 0 && t(k) > startTime(j) + b
+                tuples = fetch(ephys.Spikes(unitKey) ...
+                    * (stimulation.StimTrials(key) & 'valid_trial = true') ...
+                    * nc.SpikeCountParams(key), 'start_time');
+                spikeCount = cell(numel(tuples), 1);
+                trial = 1;
+                for tuple = tuples'
+                    while k > 0 && t(k) > tuple.start_time + b
                         k = k - 1;
                     end
-                    while k < nSpikes && t(k + 1) < startTime(j) + b
+                    while k < nSpikes && t(k + 1) < tuple.start_time + b
                         k = k + 1;
                     end
                     k0 = k;
-                    while k < nSpikes && t(k + 1) < startTime(j) + e
+                    while k < nSpikes && t(k + 1) < tuple.start_time + e
                         k = k + 1;
                     end
-                    spikeCount(j) = k - k0;
+                    spikeCount{trial} = k - k0;
+                    trial = trial + 1;
                 end
-                key.unit_id = unitId(i);
-                trials = struct('trial_num', num2cell(trialNum), 'spike_count', num2cell(spikeCount));
-                tuples = dj.utils.structJoin(key, trials);
+                tuples = rmfield(tuples, 'start_time');
+                [tuples.spike_count] = deal(spikeCount{:});
                 insert(nc.SpikeCounts, tuples);
             end
         end
