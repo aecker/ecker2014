0f7a062f06870eb78d8d156a6034f2179a0c6113
Alexander Ecker <aecker@tuebingen.mpg.de>
2012-03-16 18:59:00 +0100

    Modified quicknc() to use NoiseCorrelation table

--- quicknc.m
+++ quicknc.m
@@ -1,25 +1,8 @@
 function r = quicknc()
 
-subject.subject_id = 9;
-stimKeys = fetch(nc.Gratings(subject) & nc.SpikeCountSet);
-for stimKey = stimKeys'
-    condKeys = fetch(nc.GratingConditions(stimKey));
-    nCond = numel(condKeys);
-    excludeKeys = fetch(ephys.SingleUnit(subject) & 'fp + fn > 0.1' & nc.SpikeCountSet(stimKey));
-    pairKeys = fetch(nc.UnitPairs(stimKey) - (nc.UnitPairMembership & excludeKeys));
-    nPairs = numel(pairKeys);
-    r = zeros(nPairs, nCond);
-    for i = 1:nCond
-        unitKeys = fetch(ephys.SingleUnit(subject) & (nc.UnitPairMembership * nc.UnitPairs(pairKeys)));
-        nUnits = numel(unitKeys);
-        x = zeros(100, nUnits);
-        for j = 1:nUnits
-            x(:,j) = fetchn(nc.SpikeCounts(unitKeys(j)) & nc.GratingTrials(condKeys(i)), 'spike_count');
-        end
-        R = corrcoef(x);
-        r(:,i) = R(~tril(ones(nUnits)));
-    end
-end
+key.subject_id = 9;
+key.sort_method_num = 2;
 
-
-
+exclude = ephys.SingleUnit(key) & 'fp + fn > 0.05';
+rel = nc.NoiseCorrelations(key) - (nc.UnitPairMembership & exclude);
+r = fetchn(rel, 'r_noise_avg');
