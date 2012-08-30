894fd86a6dbeea944c5f26f9e695d4a4a13e50bb
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-08-30 16:33:14 +0200

    Added distance to PairStats

--- +nc/PairStats.m
+++ +nc/PairStats.m
@@ -8,6 +8,7 @@ geom_mean_rate  : double    # average geometric mean firing rate
 min_rate        : double    # average minimum firing rate
 diff_pref_ori   : double    # difference in preferred orientation
 r_signal        : double    # signal correlations
+distance        : double    # distance between cells (tetrodes)
 %}
 
 classdef PairStats < dj.Relvar
@@ -22,12 +23,14 @@ classdef PairStats < dj.Relvar
         
         function makeTuples(self, key)
             [rates, pref] = fetchn(nc.OriTuning * nc.UnitPairMembership(key), 'dir_mean_rate', 'pref_ori');
+            [x, y] = fetchn(nc.UnitPairMembership(key) * ephys.Spikes * ae.TetrodeProperties, 'loc_x', 'loc_y');
             rates = cat(1, rates{:});
             tuple = key;
             tuple.geom_mean_rate = mean(sqrt(prod(rates, 1)));
             tuple.min_rate = mean(min(rates, [], 1));
             tuple.diff_pref_ori = abs(angle(exp(2i * (pref(1) - pref(2))))) / 2;
             tuple.r_signal = corr(rates(1, :)', rates(2, :)');
+            tuple.distance = sqrt(diff(x).^2 + diff(y).^2);
             self.insert(tuple);
         end
     end
