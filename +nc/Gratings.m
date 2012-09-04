8c0065c4163611adeae4cc93a398abd65e9c3578
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-09-04 11:56:39 +0200

    Updated Gratings to work with old MPI data

--- +nc/Gratings.m
+++ +nc/Gratings.m
@@ -15,7 +15,7 @@ classdef Gratings < dj.Relvar & dj.AutoPopulate
     properties(Constant)
         table = dj.Table('nc.Gratings');
         popRel = stimulation.StimTrialGroup * ...
-            acq.Stimulation('exp_type IN ("GratingExperiment", "AcuteGratingExperiment")');
+            acq.Stimulation('exp_type IN ("GratingExperiment", "AcuteGratingExperiment", "mgrad", "movgrad")');
     end
     
     methods 
@@ -26,6 +26,13 @@ classdef Gratings < dj.Relvar & dj.AutoPopulate
 
     methods(Access = protected)
         function makeTuples(this, key)
+            
+            % catch old legacy datasets
+            if any(strcmp(fetch1(acq.Stimulation(key), 'exp_type'), {'mgrad', 'movgrad'}))
+                makeTuplesMPI(this, key)
+                return
+            end
+            
             % Grating session
             tuple = key;
             rel = stimulation.StimTrialGroup(key);
@@ -59,6 +66,48 @@ classdef Gratings < dj.Relvar & dj.AutoPopulate
             condition = getParam(stimulation.StimTrials(key), 'condition', 'UniformOutput', false);
             [trials.condition_num] = deal(condition{:});
             insert(nc.GratingTrials, trials);
+        end
+    end
+    
+    methods (Access = private)
+        function makeTuplesMPI(this, key)
+            % Import old MPI data (separate to keep makeTuples clean)
+            
+            expType = fetch1(acq.Stimulation(key), 'exp_type');
+            
+            % Grating session
+            tuple = key;
+            rel = stimulation.StimTrialGroup(key);
+            tuple.speed = 3.4 * isequal(expType, 'movgrad');
+            tuple.location_x = getConstant(rel, 'xOffset');
+            tuple.location_y = getConstant(rel, 'yOffset');
+            tuple.spatial_freq = 4;
+            tuple.stimulus_time = getConstant(rel, 'stimulusTime');
+            tuple.post_stimulus_time = 0;
+            diskSize = getConstant(rel, 'diskSize');
+            insert(this, tuple);
+            
+            % Conditions
+            rel = stimulation.StimConditions(key);
+            direction = getConditionParam(rel, 'orientation');
+            contrast = getConditionParam(rel, 'contrast');
+            conditions = fetch(rel);
+            for i = 1:numel(conditions)
+                conditions(i).orientation = mod(direction(i), 180);
+                conditions(i).direction = direction(i);
+                conditions(i).contrast = contrast(i);
+                conditions(i).disk_size = diskSize;
+                conditions(i).initial_phase = 0;
+            end
+            insert(nc.GratingConditions, conditions);
+            
+            % Trials
+            trials = fetch(stimulation.StimTrials(key));
+            condition = getParam(stimulation.StimTrials(key), 'condition');
+            condition(isnan(condition)) = 1;
+            condition = num2cell(condition);
+            [trials.condition_num] = deal(condition{:});
+            insert(nc.GratingTrials, trials);            
         end
     end
 end
