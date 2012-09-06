f9990eda8eb28eb907443edae95c76ab4b8de4d6
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-09-06 16:13:21 +0200

    added r_signal and distance dependence plots. still need improvement and 2d plots

--- corrstruct/analyses.m
+++ corrstruct/analyses.m
@@ -14,13 +14,39 @@ d = d(ndx);
 rs = rs(ndx);
 
 
-%% plots
+%% firing rate dependence
 bins = 10 .^ (-2.5 : 0.25 : 2);
 bins(1) = 0;
-[count, bin] = histc(fr, bins);
+[count, bin] = histc(fr, bins); %#ok
 sz = [numel(bins) - 1, 1];
 m = accumarray(bin, r, sz, @mean);
 se = accumarray(bin, r, sz, @(x) std(x) / sqrt(numel(x)));
 
 binCenters = log10(bins(2 : end)) - diff(log10(bins(2 : 3))) / 2;
 errorbar(binCenters, m, se, '.-')
+
+
+%% signal correlation dependence
+bins = -1 : 0.25 : 1;
+bins(end) = 1.001;
+[count, bin] = histc(rs, bins); %#ok
+sz = [numel(bins) - 1, 1];
+m = accumarray(bin, r, sz, @mean);
+se = accumarray(bin, r, sz, @(x) std(x) / sqrt(numel(x)));
+
+binCenters = bins(2 : end) - diff(bins(1 : 2)) / 2;
+errorbar(binCenters, m, se, '.-')
+
+
+%% distance dependence
+bins = 0 : 0.18 : 0.72;
+% bins = 0 : 0.5 : 4;
+[count, bin] = histc(d, bins);
+sz = [numel(bins) - 1, 1];
+m = accumarray(bin, r, sz, @mean);
+se = accumarray(bin, r, sz, @(x) std(x) / sqrt(numel(x)));
+
+binCenters = bins(2 : end) - diff(bins(1 : 2)) / 2;
+errorbar(binCenters, m, se, '.-')
+
+
