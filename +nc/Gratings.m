c16ab7500b70ab604aca64a0229e900dae837c2a
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-09-07 20:27:30 +0200

    Added function to return all valid trials which are in fully completed blocks

--- +nc/Gratings.m
+++ +nc/Gratings.m
@@ -22,6 +22,23 @@ classdef Gratings < dj.Relvar & dj.AutoPopulate
         function self = Gratings(varargin)
             self.restrict(varargin{:})
         end
+        
+        
+        function trials = validTrialsCompleteBlocks(self)
+            % Returns all valid trials in all completed blocks
+            %   This function excludes the last couple of trials if the
+            %   last experimental blocks wasn't fully completed (so that
+            %   there is an equal number of trials for each condition).
+            
+            assert(count(self) == 1, 'Relvar must be scalar!')
+            nCond = count(nc.GratingConditions & self);
+            validTrials = stimulation.StimTrials('valid_trial = true') & self;
+            nTrials = count(validTrials);
+            trialNums = sort(fetchn(validTrials, 'trial_num'));
+            lastTrial = trialNums(fix(nTrials / nCond) * nCond);
+            restriction = sprintf('valid_trial = true AND trial_num <= %d', lastTrial);
+            trials = self * stimulation.StimTrials(restriction) * nc.GratingTrials;
+        end
     end
 
     methods(Access = protected)
