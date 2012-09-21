4528e8ee6033c54dc4dc0ee1a5e36fb2fb791a37
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-09-21 18:37:20 +0200

    couple of small changes to analysis script

--- corrstruct/analyses.m
+++ corrstruct/analyses.m
@@ -1,10 +1,14 @@
 % Dependence of noise correlations on firing rates
 % AE 2012-08-06
 
-key = struct('subject_id', {23}, 'sort_method_num', 5, 'spike_count_end', 500);
-excludePairs = nc.UnitPairMembership(key) & ((ephys.SingleUnit(key) & 'fp + fn > 0.1') + (nc.UnitStats(key) & 'stability > 0.1'));
-% excludePairs = nc.UnitPairMembership(key) & ((ephys.SingleUnit(key) & 'fp + fn > 1'));
+key = struct('subject_id', {8}, 'sort_method_num', 5, 'spike_count_end', 500);
+assert(~count((nc.UnitPairMembership(key) & nc.NoiseCorrelationSet) - nc.UnitStats(key)), ...
+    'nc.UnitStats need to be completely populated!')
+excludePairs = nc.UnitPairMembership(key) & ( ...
+    (ephys.SingleUnit(key) & 'fp + fn > 0.1') + ...
+    (nc.UnitStats(key) & 'stability > 0.1'));
 [r, fr, d, rs] = fetchn(nc.NoiseCorrelations(key) - excludePairs, 'r_noise_avg', 'min_rate', 'distance', 'r_signal');
+% [r, fr, d, rs] = fetchn((nc.NoiseCorrelations(key) & 'distance=0') - excludePairs, 'r_noise_avg', 'min_rate', 'distance', 'r_signal');
 
 % throw out nans
 ndx = ~isnan(r);
@@ -17,7 +21,7 @@ rs = rs(ndx);
 %% firing rate dependence
 bins = 10 .^ (-2.5 : 0.25 : 2);
 bins(1) = 0;
-[count, bin] = histc(fr, bins); %#ok
+[counts, bin] = histc(fr, bins); %#ok
 sz = [numel(bins) - 1, 1];
 m = accumarray(bin, r, sz, @mean);
 se = accumarray(bin, r, sz, @(x) std(x) / sqrt(numel(x)));
@@ -27,9 +31,9 @@ errorbar(binCenters, m, se, '.-')
 
 
 %% signal correlation dependence
-bins = -1 : 0.25 : 1;
+bins = -1 : 0.5 : 1;
 bins(end) = 1.001;
-[count, bin] = histc(rs, bins); %#ok
+[counts, bin] = histc(rs, bins); %#ok
 sz = [numel(bins) - 1, 1];
 m = accumarray(bin, r, sz, @mean);
 se = accumarray(bin, r, sz, @(x) std(x) / sqrt(numel(x)));
@@ -39,9 +43,9 @@ errorbar(binCenters, m, se, '.-')
 
 
 %% distance dependence
-bins = 0 : 0.18 : 0.72;
-% bins = 0 : 0.5 : 4;
-[count, bin] = histc(d, bins);
+% bins = 0 : 0.18 : 0.72;
+bins = 0 : 0.5 : 4;
+[counts, bin] = histc(d, bins);
 sz = [numel(bins) - 1, 1];
 m = accumarray(bin, r, sz, @mean);
 se = accumarray(bin, r, sz, @(x) std(x) / sqrt(numel(x)));
