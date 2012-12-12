e317e59a34b81232d317240ebc601674fb21ca0a
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-12-12 10:57:02 +0100

    Added residual covariances after accounting for internal factors

--- +nc/GpfaCovExpl.m
+++ +nc/GpfaCovExpl.m
@@ -6,6 +6,8 @@ nc.GpfaCovExpl (computed) # Covariance explained by GPFA model
 cov_train           : mediumblob    # covariance matrix of training set
 cov_test            : mediumblob    # covariance matrix of test set
 cov_pred            : mediumblob    # predicted covariance matrix
+cov_resid_train     : mediumblob    # residual covariance for training set
+cov_resid_test      : mediumblob    # residual covariance for test set
 norm_train          : double        # norm for training set
 norm_test           : double        # norm for tes set
 norm_pred           : double        # norm for prediction
@@ -31,13 +33,17 @@ classdef GpfaCovExpl < dj.Relvar & dj.AutoPopulate
         function makeTuples(self, key)
             Y = fetch1(nc.GpfaModelSet(key), 'transformed_data');
             [train, test, model] = fetch1(nc.GpfaModel(key), 'train_set', 'test_set', 'model');
+            model = GPFA(model);
             Qtrain = cov(Ysub(Y, train));
             Qtest = cov(Ysub(Y, test));
             Qpred = model.C * model.C' + model.R;
+            Yres = model.resid(Y);
             tuple = key;
             tuple.cov_train = Qtrain;
             tuple.cov_test = Qtest;
             tuple.cov_pred = Qpred;
+            tuple.cov_resid_train = cov(Ysub(Yres, train));
+            tuple.cov_resid_test = cov(Ysub(Yres, test));
             tuple.norm_diff_train = norm(Qtrain - Qpred, 'fro');
             tuple.norm_diff_test = norm(Qtest - Qpred, 'fro');
             tuple.norm_train = norm(Qtrain, 'fro');
--- +nc/GpfaCovExplPairs.m
+++ +nc/GpfaCovExplPairs.m
@@ -8,6 +8,8 @@ unit_j          : tinyint unsigned  # unit j
 train_ij        : double            # covariance matrix of training set
 test_ij         : double            # covariance matrix of test set
 pred_ij         : double            # predicted covariance matrix
+resid_train_ij  : dobule            # residual covariance for training set
+resid_test_ij   : dobule            # residual covariance for test set
 %}
 
 classdef GpfaCovExplPairs < dj.Relvar
@@ -21,8 +23,8 @@ classdef GpfaCovExplPairs < dj.Relvar
         end
         
         function makeTuples(self, key)
-            [Qtrain, Qtest, Qpred, unitIds] = fetch1(nc.GpfaModelSet * nc.GpfaCovExpl & key, ...
-                'cov_train', 'cov_test', 'cov_pred', 'unit_ids');
+            [Qtrain, Qtest, Qpred, QresTrain, QresTest, unitIds] = fetch1(nc.GpfaModelSet * nc.GpfaCovExpl & key, ...
+                'cov_train', 'cov_test', 'cov_pred', 'cov_resid_train', 'cov_resid_test', 'unit_ids');
             for i = 1 : numel(unitIds)
                 for j = i + 1 : numel(unitIds)
                     tuple = key;
@@ -31,6 +33,8 @@ classdef GpfaCovExplPairs < dj.Relvar
                     tuple.train_ij = Qtrain(i, j);
                     tuple.test_ij = Qtest(i, j);
                     tuple.pred_ij = Qpred(i, j);
+                    tuple.resid_train_ij = QresTrain(i, j);
+                    tuple.resid_test_ij = QresTest(i, j);
                     self.insert(tuple);
                 end
             end
