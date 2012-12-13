97e801a65b6109da38ff78598bb2e3a474c02f06
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-12-13 16:06:41 +0100

    changed pairwise covariance explained analysis to use GpfaCovExpl table instead of deleted GpfaCovExplPairs

--- gpfa/covExplPairwise.m
+++ gpfa/covExplPairwise.m
@@ -23,42 +23,67 @@ else
     restrictions = varargin;
 end
 
-rel = nc.GpfaCovExplPairs & restrictions;
+rel = nc.GpfaCovExpl & restrictions;
 n = count(rel & 'latent_dim = 0');
+pmax = count(rel) / n - 1;
+data = fetch(rel, '*');
+kfold = data(1).kfold_cv;
 
-pmax = 10;
-dtrain = zeros(n, pmax + 1);
-dtest = zeros(n, pmax + 1);
-for p = 0 : pmax
-    [dtrain(:, p + 1), dtest(:, p + 1)] = fetchn(rel & struct('latent_dim', p), ...
-        'train_ij - pred_ij -> train', 'test_ij - pred_ij -> test');
-end
+% some sanity checks
+assert(numel(unique([data.sort_method_num])) == 1, 'sort_method_num must be specified!')
+assert(numel(unique([data.bin_size])) == 1, 'bin_size must be specified!')
+assert(numel(unique([data.kfold_cv])) == 1, 'kfold_cv must be specified!')
+assert(numel(unique([data.transform_num])) == 1, 'transform_num must be specified!')
+
+data = dj.struct.sort(data, {'cv_run', 'latent_dim', 'stim_start_time'});
+data = reshape(data, [n / kfold, pmax + 1, kfold]);
+
+dtrain = differences(data, 'cov_train');
+dtest = differences(data, 'cov_test');
 
 % plot data
 figure(20 + data(1).transform_num), clf
 
 subplot(2, 2, 1)
-plot(0 : pmax, sqrt(mean(dtrain .^ 2, 1)), '.-k')
+plot(0 : pmax, sqrt(mean(mean(dtrain .^ 2, 3), 1)), '.-k')
 xlim([-1 pmax + 1])
 title('RMS difference')
 set(legend('Training data'), 'box', 'off')
 box off
 
 subplot(2, 2, 2)
-plot(0 : pmax, median(abs(dtrain), 1), '.-k')
+plot(0 : pmax, median(mean(abs(dtrain), 3), 1), '.-k')
 xlim([-1 pmax + 1])
 title('median absolute difference')
 box off
 
 subplot(2, 2, 3)
-plot(0 : pmax, sqrt(mean(dtest .^ 2, 1)), '.-r')
+plot(0 : pmax, sqrt(mean(mean(dtest, 3), 1)), '.-r')
 xlim([-1 pmax + 1])
 xlabel('# latent factors')
 set(legend('Test data'), 'box', 'off')
 box off
 
 subplot(2, 2, 4)
-plot(0 : pmax, median(abs(dtest), 1), '.-r')
+plot(0 : pmax, median(mean(abs(dtest), 3), 1), '.-r')
 xlim([-1 pmax + 1])
 xlabel('# latent factors')
 box off
+
+
+
+function d = differences(data, field)
+
+offdiag = @(x) x(~tril(ones(size(x))));
+[n, pmax, kfold] = size(data);
+d = zeros(0, pmax);
+for p = 1 : pmax
+    for k = 1 : kfold
+        dpk = cell(1, n);
+        for i = 1 : n
+            dpk{i} = offdiag(data(i, p, k).(field) - data(i, p, k).cov_pred);
+        end
+        dpk = cat(1, dpk{:});
+        d(1 : numel(dpk), p, k) = dpk;
+    end
+end
