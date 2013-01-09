ce75c98197fe721440ce86bcb6b787d7b60c9e67
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-01-09 09:56:55 +0100

    Added results for z-scored data

--- gpfa/analyzeTransforms.m
+++ gpfa/analyzeTransforms.m
@@ -2,21 +2,20 @@ function analyzeTransforms()
 % Evaluate performance of different transformations.
 %   analyzeTransforms()
 %
-%   Here I compute the R^2 (variance explained) of the one-factor GPFA
-%   model for each cell as a function of the various data transformations
-%   that I used for fitting.
+%   Here I compute the R^2 between predicted and observed data for the
+%   one-factor GPFA model for each cell as a function of the various data
+%   transformations that I used for fitting.
 %
 %   * The differences between the transforms aren't too big (ca. 10%)
 %   * Anscombe transform and log(x + 1) perform best
 %   * R^2 is larger for higher firing rates. This is particularly strong
 %       for the untransformed data. It seems like the model puts most of
-%       its weight here on explaining the high-firing-rate cells. This is
-%       of course also somewhat of a concern for the other transforms if
-%       variances are still higher there for high-firing cells.
-%
-%   TODO: look also at z-scored data, which eliminates the concern that the
-%         high firing rates dominate. Of course this has the opposite
-%         concern that sparsely firing cells' variances will be blown up.
+%       its weight here on explaining the high-firing-rate cells.
+%   * The model does put a bit more weight on the most active cells, which
+%       is evident when looking at z-scored data: R^2 is reduced somewhat
+%       for the most active cells, although not by too much. R^2 remains
+%       larger for the most active cells. Thus, the model isn't _just_
+%       putting large weight on the most active cells.
 %   
 % AE 2012-12-21
 
@@ -25,49 +24,54 @@ restrictions = {'subject_id IN (9, 11) AND sort_method_num = 5', struct('kfold_c
 latent = 'latent_dim = 1';
 
 nTrans = count(nc.DataTransforms);
-nUnits = count(nc.GpfaUnits & restrictions) / nTrans;
-rsq = zeros(nTrans, nUnits);
-mfr = zeros(nTrans, nUnits);
+nUnits = count(nc.GpfaUnits & restrictions) / nTrans / 2;
+rsq = zeros(nUnits, nTrans, 2);
+mfr = zeros(nUnits, nTrans, 2);
 tr = 1;
 
 % Compute R^2 for different transforms
 for transform = fetch(nc.DataTransforms)'
-    unit = 0;
-    for modelset = fetch(nc.GpfaModelSet & restrictions & transform, 'transformed_data', 'raw_data', 'bin_size')'
-        rsqi = 0;
-        mfri = 0;
-        for run = fetch(nc.GpfaModel & modelset & latent, 'model', 'test_set')'
-            model = GPFA(run.model);
-            Y = modelset.transformed_data(:, :, run.test_set);
-            Ypred = model.predict(Y);
-            Y = Y(1 : end, :)';
-            Ypred = Ypred(1 : end, :)';
-            rsqi = rsqi + mean(zscore(Y, 1) .* zscore(Ypred, 1), 1) .^ 2;
-            Yraw = modelset.raw_data(:, :, run.test_set);
-            mfri = mfri + 1000 / modelset.bin_size * mean(mean(Yraw, 2), 3)';
+    for z = [0 1]
+        unit = 0;
+        for modelset = fetch(nc.GpfaModelSet & restrictions & transform & struct('zscore', z), ...
+                'transformed_data', 'raw_data', 'bin_size')'
+            rsqi = 0;
+            mfri = 0;
+            for run = fetch(nc.GpfaModel & modelset & latent, 'model', 'test_set')'
+                model = GPFA(run.model);
+                Y = modelset.transformed_data(:, :, run.test_set);
+                Ypred = model.predict(Y);
+                Y = Y(1 : end, :)';
+                Ypred = Ypred(1 : end, :)';
+                rsqi = rsqi + mean(zscore(Y, 1) .* zscore(Ypred, 1), 1) .^ 2;
+                Yraw = modelset.raw_data(:, :, run.test_set);
+                mfri = mfri + 1000 / modelset.bin_size * mean(mean(Yraw, 2), 3)';
+            end
+            n = numel(rsqi);
+            rsq(unit + (1 : n), tr, z + 1) = rsqi / kfold;
+            mfr(unit + (1 : n), tr, z + 1) = mfri / kfold;
+            unit = unit + n;
         end
-        n = numel(rsqi);
-        rsq(tr, unit + (1 : n)) = rsqi / kfold;
-        mfr(tr, unit + (1 : n)) = mfri / kfold;
-        unit = unit + n;
     end
     tr = tr + 1;
 end
 
 % scatter plots: R^2 vs. firing rate
-figure(1), clf
 bb = -0.5 : 0.5 : 2.5;
 bins = 10 .^ bb;
 binsc = 10 .^ (bb(1 : end - 1) + diff(bb(1 : 2)) / 2);
-for i = 1 : nTrans
-    subplot(2, 2, i)
-    plot(mfr(i, :), rsq(i, :), '.k', 'markersize', 1)
-    hold on
-    [~, bin] = histc(mfr(i, :), bins);
-    m = accumarray(bin', rsq(i, :)', [numel(bins) - 1, 1], @mean);
-    plot(binsc, m, '*-r') 
-    set(gca, 'xscale', 'log', 'yscale', 'linear', 'box', 'off')
-    axis tight
+for z = 1 : 2
+    figure(10 + z), clf
+    for i = 1 : nTrans
+        subplot(2, 2, i)
+        plot(mfr(:, i, z), rsq(:, i, z), '.k', 'markersize', 1)
+        hold on
+        [~, bin] = histc(mfr(:, i, z), bins);
+        m = accumarray(bin, rsq(:, i, z), [numel(bins) - 1, 1], @mean);
+        plot(binsc, m, '*-r')
+        set(gca, 'xscale', 'log', 'yscale', 'linear', 'box', 'off')
+        axis tight
+    end
 end
 
 % Summary plot
@@ -77,16 +81,21 @@ figure(2), clf
 subplot(2, 1, 1), hold all
 colors = colormap(lines);
 for i = 1 : nTrans
-    bar(i, mean(rsq(i, :)), 0.5, 'facecolor', colors(i, :))
+    for z = 1 : 2
+        bar((i - 1) * 2 + z, mean(rsq(:, i, z)), 0.5, 'facecolor', colors(i, :))
+    end
 end
-set(gca, 'xlim', [0 tr], 'xtick', 1 : tr - 1, 'box', 'off', ...
+set(gca, 'xlim', [0, 2 * tr - 1], 'xtick', 1.5 : 2 : 2 * (tr - 1), 'box', 'off', ...
     'xticklabel', fetchn(nc.DataTransforms, 'name'))
 ylabel('Average R^2')
 subplot(2, 1, 2), hold all
+linestyle = {'.-', '.--'};
 for i = 1 : nTrans
-    [~, bin] = histc(mfr(i, :), bins);
-    m = accumarray(bin', rsq(i, :)', [numel(bins) - 1, 1], @mean);
-    plot(binsc, m, '.-', 'color', colors(i, :)) 
+    for z = 1 : 2
+        [~, bin] = histc(mfr(:, i, z), bins);
+        m = accumarray(bin, rsq(:, i, z), [numel(bins) - 1, 1], @mean);
+        plot(binsc, m, linestyle{z}, 'color', colors(i, :))
+    end
 end
 set(gca, 'xscale', 'log', 'yscale', 'linear', 'box', 'off', 'xlim', [0.5 200])
 set(gca, 'xticklabel', get(gca, 'xtick'))
