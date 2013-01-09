0ec03087b7e9ab673c03ed848c9aabdaeb4be86e
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-01-09 15:38:31 +0100

    Added option to analyze spike counts in entire trial. Also removed some old fields that aren't used any more.

--- +nc/GpfaCovExpl.m
+++ +nc/GpfaCovExpl.m
@@ -2,14 +2,13 @@
 nc.GpfaCovExpl (computed) # Covariance explained by GPFA model
 
 -> nc.GpfaModel
+by_trial            : boolean       # use spike counts for entire trial
 ---
 cov_train           : mediumblob    # covariance matrix of training set
 cov_test            : mediumblob    # covariance matrix of test set
 cov_pred            : mediumblob    # predicted covariance matrix
 cov_resid_train     : mediumblob    # residual covariance for training set
 cov_resid_test      : mediumblob    # residual covariance for test set
-cov_resid_raw_train : mediumblob    # resid cov for untrans data (training)
-cov_resid_raw_test  : mediumblob    # resid cov for untrans data (test)
 norm_train          : double        # norm for training set
 norm_test           : double        # norm for tes set
 norm_pred           : double        # norm for prediction
@@ -22,7 +21,7 @@ rel_diff_test       : double        # relative difference for test set
 classdef GpfaCovExpl < dj.Relvar & dj.AutoPopulate
     properties(Constant)
         table = dj.Table('nc.GpfaCovExpl');
-        popRel = nc.GpfaModel;
+        popRel = nc.GpfaModelSet;
     end
     
     methods 
@@ -33,39 +32,52 @@ classdef GpfaCovExpl < dj.Relvar & dj.AutoPopulate
     
     methods (Access = protected)
         function makeTuples(self, key)
-            [Y, Yt] = fetch1(nc.GpfaModelSet(key), 'raw_data', 'transformed_data');
-            [train, test, model] = fetch1(nc.GpfaModel(key), 'train_set', 'test_set', 'model');
-            model = GPFA(model);
-            Qtrain = cov(Ysub(Yt, train));
-            Qtest = cov(Ysub(Yt, test));
-            Qpred = model.C * model.C' + model.R;
-            Ypred = model.predict(Yt);
-            Yres = Yt - Ypred;
-            YpredRaw = invert(nc.DataTransforms & key, Ypred);
-            YresRaw = Y - YpredRaw;
-            tuple = key;
-            tuple.cov_train = Qtrain;
-            tuple.cov_test = Qtest;
-            tuple.cov_pred = Qpred;
-            tuple.cov_resid_train = cov(Ysub(Yres, train));
-            tuple.cov_resid_test = cov(Ysub(Yres, test));
-            tuple.cov_resid_raw_train = cov(Ysub(YresRaw, train));
-            tuple.cov_resid_raw_test = cov(Ysub(YresRaw, test));
-            tuple.norm_diff_train = norm(Qtrain - Qpred, 'fro');
-            tuple.norm_diff_test = norm(Qtest - Qpred, 'fro');
-            tuple.norm_train = norm(Qtrain, 'fro');
-            tuple.norm_test = norm(Qtest, 'fro');
-            tuple.norm_pred = norm(Qpred, 'fro');
-            tuple.rel_diff_train = tuple.norm_diff_train / tuple.norm_train;
-            tuple.rel_diff_test = tuple.norm_diff_test / tuple.norm_test;
-            self.insert(tuple);
+            Yt = fetch1(nc.GpfaModelSet & key, 'transformed_data');
+            for key = fetch(nc.GpfaModel & key)'
+                [train, test, model] = fetch1(nc.GpfaModel & key, 'train_set', 'test_set', 'model');
+                model = GPFA(model);
+                for byTrial = [false true]
+                    Qtrain = cov(Ysub(Yt, train, byTrial));
+                    Qtest = cov(Ysub(Yt, test, byTrial));
+                    if byTrial
+                        Qpred = model.T * model.R;
+                        for i = 1 : model.p
+                            K = toeplitz(model.covFun(0 : model.T - 1, model.gamma(i)));
+                            Qpred = Qpred + (model.C(:, i) * model.C(:, i)') * sum(K(:));
+                        end
+                    else
+                        Qpred = model.C * model.C' + model.R;
+                    end
+                    Ypred = model.predict(Yt);
+                    Yres = Yt - Ypred;
+                    tuple = key;
+                    tuple.by_trial = byTrial;
+                    tuple.cov_train = Qtrain;
+                    tuple.cov_test = Qtest;
+                    tuple.cov_pred = Qpred;
+                    tuple.cov_resid_train = cov(Ysub(Yres, train, byTrial));
+                    tuple.cov_resid_test = cov(Ysub(Yres, test, byTrial));
+                    tuple.norm_diff_train = norm(Qtrain - Qpred, 'fro');
+                    tuple.norm_diff_test = norm(Qtest - Qpred, 'fro');
+                    tuple.norm_train = norm(Qtrain, 'fro');
+                    tuple.norm_test = norm(Qtest, 'fro');
+                    tuple.norm_pred = norm(Qpred, 'fro');
+                    tuple.rel_diff_train = tuple.norm_diff_train / tuple.norm_train;
+                    tuple.rel_diff_test = tuple.norm_diff_test / tuple.norm_test;
+                    self.insert(tuple);
+                end
+            end
         end
     end
     
 end
 
 
-function Y = Ysub(Y, index)
+function Y = Ysub(Y, index, byTrial)
     Y = Y(:, :, index);
-    Y = reshape(Y, size(Y, 1), size(Y, 2) * size(Y, 3))';
+    if byTrial
+        Y = permute(sum(Y, 2), [3 1 2]);
+    else
+        Y = Y(1 : end, :)';
+    end
 end
