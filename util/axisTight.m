2ffbc9a2852c1a3ae5ec1dc2d5471c58f89b101f
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-02-01 16:24:50 +0100

    small improvement

--- util/axisTight.m
+++ util/axisTight.m
@@ -9,7 +9,11 @@ axis tight
 ax = axis;
 x = ax(2) - ax(1);
 y = ax(4) - ax(3);
-ax = ax + p * [-x x -y y];
+if abs(ax(3)) < 10 * eps
+    ax = ax + p * [-x x 0 y];
+else
+    ax = ax + p * [-x x -y y];
+end
 axis(ax)
 if nargout
     varargout{1} = ax;
