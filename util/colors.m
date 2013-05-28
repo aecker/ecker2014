80242a1a148f5bb11f5dd0f8f8bffa386404df85
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-05-28 15:44:47 +0200

    Fixed colors

--- util/colors.m
+++ util/colors.m
@@ -2,7 +2,7 @@ function c = colors(state)
 
 switch state
     case 'awake'
-        c = [1 0 0];
-    case 'anesthetized'
         c = [0 0.4 1];
+    case 'anesthetized'
+        c = [1 0 0];
 end
