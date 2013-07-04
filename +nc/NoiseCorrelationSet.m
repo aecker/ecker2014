b5945bde50abc33ff2e79791daae260356865caf
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-07-04 09:44:17 +0200

    NoiseCorrelations: added r_lt, r_st, r_filt, max_stability and max_contam

--- +nc/NoiseCorrelationSet.m
+++ +nc/NoiseCorrelationSet.m
@@ -5,73 +5,91 @@ nc.NoiseCorrelationSet (computed) # Set of all noise correlations
 -> ae.SpikeCountSet
 -> nc.OriTuningSet
 -> nc.Gratings
+-> nc.UnitStatsSet
 ---
 %}
 
 classdef NoiseCorrelationSet < dj.Relvar & dj.AutoPopulate
     properties(Constant)
         table = dj.Table('nc.NoiseCorrelationSet');
-        popRel = (nc.UnitPairSet * ae.SpikeCountSet * nc.OriTuningSet * nc.Gratings) ...
+        popRel = (nc.UnitPairSet * ae.SpikeCountSet * nc.OriTuningSet * nc.Gratings * nc.UnitStatsSet) ...
             & 'stimulus_time + 30 >= spike_count_end';
-    end
-    
-    methods 
-        function self = NoiseCorrelationSet(varargin)
-            self.restrict(varargin{:})
-        end
     end
 
     methods(Access = protected)
-        function makeTuples(this, key)
-            insert(this, key);
+        function makeTuples(self, key)
+            self.insert(key);
             
-            % compute noise correlations for all pairs and conditions
-            condKeys = fetch(nc.GratingConditions(key));
+            % get trials, conditions, pairs, etc.
+            condKeys = fetch(nc.GratingConditions & key);
             nCond = numel(condKeys);
-            pairKeys = fetch(nc.UnitPairs(key));
+            pairKeys = fetch(nc.UnitPairs & key);
             nPairs = numel(pairKeys);
-            nUnits = count(ephys.Spikes(key));
+            nUnits = count(ephys.Spikes & key);
+            unitIds = fetchn(nc.UnitPairs * nc.UnitPairMembership & key, 'unit_id', 'ORDER BY pair_num');
+            unitIds = reshape(unitIds, 2, []);
+            counts = fetchn(ae.SpikeCounts & key, 'spike_count', 'ORDER BY unit_id, trial_num');
+            counts = reshape(counts, [], nUnits);
+            cond = fetchn(nc.GratingTrials * stimulation.StimTrials & key & 'valid_trial = true', 'condition_num', 'ORDER BY trial_num');
+            
+            % compute noise correlations and z-scores for each condition
             r = zeros(nCond, nPairs);
             rates = zeros(nCond, nUnits);
-            pairs = fetch(nc.UnitPairs(key) * nc.UnitPairMembership);
-            pairs = dj.struct.sort(pairs, 'pair_num');
-            unitIds = reshape([pairs.unit_id], 2, []);
+            z = counts;
             for iCond = 1 : nCond
-                scTuples = fetch(ae.SpikeCounts(key) & nc.GratingTrials(condKeys(iCond)), 'spike_count');
-                scTuples = dj.struct.sort(scTuples, {'unit_id', 'trial_num'});
-                spikeCounts = reshape([scTuples.spike_count], [], nUnits);
-                R = corrcoef(spikeCounts);
+                ndx = (cond == iCond);
+                R = corrcoef(counts(ndx, :));
+                z(ndx, :) = zscore(counts(ndx, :), 1, 1);
                 for iPair = 1 : nPairs
                     r(iCond, iPair) = R(unitIds(1, iPair), unitIds(2, iPair));
                 end
-                rates(iCond, :) = mean(spikeCounts, 1);
+                rates(iCond, :) = mean(counts(ndx, :), 1);
             end
             duration = key.spike_count_end - key.spike_count_start;
             rates = rates / duration * 1000;
             
-            % summary statistics for the cells
-            highContrast = fetch1(nc.OriTuning(key), 'max(contrast) -> c');
-            stats = fetch(ephys.Spikes(key) * ae.TetrodeProperties * ...
-                nc.OriTuning(struct('contrast', highContrast)), ...
-                'loc_x', 'loc_y', 'pref_ori');
-            stats = dj.struct.sort(stats, 'unit_id');
-            x = [stats.loc_x];
-            y = [stats.loc_y];
-            pref = [stats.pref_ori];
+            % high-pass filter z-scores
+            [b, a] = butter(5, 1 / 50, 'high');
+            Rf = corrcoef(filtfilt(b, a, z));
             
-            for iPair = 1 : nPairs
+            % summary statistics for the cells
+            highContrast = fetch1(nc.OriTuning & key, 'max(contrast) -> c');
+            if strcmp(fetch1(sort.Methods & key, 'sort_method_name'), 'MultiUnit')
+                rel = ephys.Spikes * ae.TetrodeProperties * nc.OriTuning * nc.UnitStats;
+                [x, y, pref, instab] = fetchn(rel & key & struct('contrast', highContrast), ...
+                    'loc_x', 'loc_y', 'pref_ori', 'tac_instability', 'ORDER BY unit_id');
+                contam = zeros(size(x));
+            else
+                rel = ephys.Spikes * ephys.SingleUnit * ae.TetrodeProperties * nc.OriTuning * nc.UnitStats;
+                [x, y, pref, instab, contam] = fetchn(rel & key & struct('contrast', highContrast), ...
+                    'loc_x', 'loc_y', 'pref_ori', 'tac_instability', 'fp + fn -> c', 'ORDER BY unit_id');
+            end
+            
+            for iPair = find(~all(isnan(r)))
                 key.pair_num = pairKeys(iPair).pair_num;
+                ui = unitIds(1, iPair);
+                uj = unitIds(2, iPair);
                 
                 tuple = key;
                 tuple.r_noise_avg = nanmean(r(:, iPair));
+                tuple.r_noise_filt = Rf(ui, uj);
                 tuple.geom_mean_rate = mean(sqrt(prod(rates(:, unitIds(:, iPair)), 2)));
                 tuple.min_rate = mean(min(rates(:, unitIds(:, iPair)), [], 2));
                 tuple.diff_pref_ori = abs(angle(exp(2i * diff(pref(unitIds(:, iPair)))))) / 2;
-                tuple.r_signal = corr(rates(:, unitIds(1, iPair)), rates(:, unitIds(2, iPair)));
-                if isnan(tuple.r_signal) % i.e. at least one cell no spikes
-                    tuple.r_signal = 0;
-                end
+                tuple.r_signal = corr(rates(:, ui), rates(:, uj));
                 tuple.distance = sqrt(diff(x(unitIds(:, iPair))) .^ 2 + diff(y(unitIds(:, iPair))) .^ 2);
+                tuple.max_instab = max(instab(ui), instab(uj));
+                tuple.max_contam = max(contam(ui), contam(uj));
+
+                % trial cross-correlogram (TCC)
+                k = 20;
+                tcc = xcorr(z(:, ui), z(:, uj), k, 'unbiased');
+                win = gausswin(2 * k + 1);
+                win(k + 1) = 0;
+                win = win / sum(win);
+                tuple.tcc = (tcc(k + 1 - (0 : k)) + tcc(k + 1 + (0 : k))) / 2;
+                tuple.r_lt = tcc' * win;
+                tuple.r_st = tcc(k + 1) - tuple.r_lt;
                 
                 insert(nc.NoiseCorrelations, tuple)
 
@@ -87,4 +105,9 @@ classdef NoiseCorrelationSet < dj.Relvar & dj.AutoPopulate
             end
         end
     end
+end
+
+
+function m = nanmean(x)
+    m = mean(x(~isnan(x)));
 end
--- +nc/NoiseCorrelations.m
+++ +nc/NoiseCorrelations.m
@@ -4,12 +4,18 @@ nc.NoiseCorrelations (computed) # Noise correlations
 -> nc.NoiseCorrelationSet
 -> nc.UnitPairs
 ---
-r_noise_avg = NULL : double    # average noise correlation
-r_signal           : double    # signal correlations
-geom_mean_rate     : double    # average geometric mean firing rate
-min_rate           : double    # average minimum firing rate
-diff_pref_ori      : double    # difference in preferred orientation
-distance           : double    # distance between cells (tetrodes)
+r_noise_avg         : double     # average noise correlation
+r_noise_filt        : double     # r_noise after highpass filtering z-scores
+r_signal            : double     # signal correlations
+r_lt                : double     # long-term component of r_noise
+r_st                : double     # short-term component of r_noise
+tcc                 : mediumblob # trial cross-correlogram
+geom_mean_rate      : double     # average geometric mean firing rate
+min_rate            : double     # average minimum firing rate
+diff_pref_ori       : double     # difference in preferred orientation
+distance            : double     # distance between cells (tetrodes)
+max_instab          : double     # max instability for pair
+max_contam          : double     # max contamination for pair
 %}
 
 classdef NoiseCorrelations < dj.Relvar
