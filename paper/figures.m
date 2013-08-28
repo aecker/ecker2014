4c378fa8789840b2852de9e06dfd492042c0dccd
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-08-28 11:59:02 +0200

    Script for reproducing all figures

---
+++ paper/figures.m
@@ -0,0 +1,17 @@
+% Figures for ther paper
+% AE 2013-08-28
+
+% Fig. 1: Rasters by neuron and tuning curves
+fig1_data
+
+% Fig. 2: Fano factors and noise correlations
+fig2_fano_nc
+
+% Fig. 3: Illustration of GPFA model and rasters by trial
+fig3_rasters
+
+% Fig. 4: Variance explained
+
+
+
+
