25839385896bb82819fd013146963df7e5cd2721
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-08-30 20:23:20 +0200

    LfpGpfaCorr: migrated to most recent version of related tables

--- +nc/LfpGpfaCorr.m
+++ +nc/LfpGpfaCorr.m
@@ -1,7 +1,7 @@
 %{
-nc.LfpGpfaCorr (imported) # LFP/GPFA correlation
+nc.LfpGpfaCorr (computed) # LFP/GPFA correlation
 
--> ae.Lfp
+-> ae.LfpSet
 -> nc.Gratings
 -> ae.SpikesByTrialSet
 -> nc.DataTransforms
@@ -15,12 +15,13 @@ corr            : double    # correlation between stimulus-subtracted LFP and GP
 p               : double    # p value
 corr_trial      : double    # correlation between trial-average-subtracted
 p_trial         : double    # p value
+xcorr_trial     : mediumblob  # cross-correlation
 %}
 
 classdef LfpGpfaCorr < dj.Relvar & dj.AutoPopulate
     properties(Constant)
         table = dj.Table('nc.LfpGpfaCorr');
-        popRel = (ae.Lfp * nc.Gratings * ae.SpikesByTrialSet * nc.DataTransforms * nc.GpfaParams) ...
+        popRel = (nc.Anesthesia * ae.LfpSet * nc.Gratings * ae.SpikesByTrialSet * nc.DataTransforms * nc.GpfaParams) ...
             & nc.GpfaModelSet & 'kfold_cv = 1 AND max_latent_dim = 1';
     end
     
@@ -35,6 +36,7 @@ classdef LfpGpfaCorr < dj.Relvar & dj.AutoPopulate
             
             modelKey = key;
             modelKey.latent_dim = 1;
+            modelKey.control = 0;
             
             stimTime = fetch1(nc.Gratings & key, 'stimulus_time');
             binSize = fetch1(nc.GpfaParams * nc.GpfaModelSet & modelKey, 'bin_size', 1);
@@ -42,10 +44,12 @@ classdef LfpGpfaCorr < dj.Relvar & dj.AutoPopulate
             nBins = round(stimTime / binSize);
             
             % extract LFP
-            [lfp, Flfp, t0] = fetch1(ae.Lfp & key, 'lfp', 'lfp_sampling_rate', 'lfp_t0');
+            rel = ae.Lfp * acq.EphysStimulationLink & (ephys.Spikes * nc.GpfaUnits & key);
+            lfp = fetchn(rel, 'lfp');
+            [Flfp, t0] = fetch1(rel, 'lfp_sampling_rate', 'lfp_t0', 'LIMIT 1');
             [p, q] = rat(Fs / Flfp, 1e-3);
             assert(p < 100 && q < 100, 'Problem with resampling LFP!')
-            lfp = resample(lfp, p, q);
+            lfp = cellfun(@(x) resample(x, p, q), lfp, 'uni', false);
             
             cond = fetchn(nc.GpfaModel & modelKey, 'condition_num');
             allTrials = sort(fetchn(nc.GratingTrials * nc.GpfaModel & modelKey, 'trial_num'));
@@ -60,10 +64,11 @@ classdef LfpGpfaCorr < dj.Relvar & dj.AutoPopulate
                 [~, trials] = ismember(trials, allTrials);
                 
                 % remove stimulus-evoked component from LFP
-                showStims = sort(fetchn(stimulation.StimTrialEvents * nc.GratingTrials...
-                    & condKey & 'event_type = "showStimulus"', 'event_time'));
-                ndx = bsxfun(@plus, round((showStims - t0) * Fs / 1000), 1 : nBins)';
-                Zi = lfp(ndx);
+                showStims = sort(double(fetchn(stimulation.StimTrialEvents * nc.GratingTrials...
+                    & condKey & 'event_type = "showStimulus"', 'event_time')));
+                ndx = bsxfun(@plus, round((showStims + 30 + binSize / 2 - t0) * Fs / 1000), 1 : nBins)';
+                Zi = cellfun(@(x) x(ndx), lfp, 'uni', false);
+                Zi = mean(cat(3, Zi{:}), 3);
                 Zi = bsxfun(@minus, Zi, mean(Zi, 2));
                 Z(:, trials) = Zi;
 
@@ -91,7 +96,24 @@ classdef LfpGpfaCorr < dj.Relvar & dj.AutoPopulate
             [rho, p] = corr(Xt(:), Zt(:));
             tuple.corr_trial = rho;
             tuple.p_trial = p;
+            tuple.xcorr_trial = xcorr(Xt, Zt);
             self.insert(tuple);
         end
     end
+end
+
+
+function z = xcorr(x, y)
+
+x = zscore(x, 1, 1);
+y = zscore(y, 1, 1);
+n = size(x, 1);
+j = 1 : n;
+z = zeros(2 * n - 1, 1);
+for k = -n + 1 : n - 1
+    i = j + k;
+    ndx = i > 0 & i <= n & j > 0 & j <= n;
+    z(n + k) = mean(mean(x(i(ndx), :) .* y(j(ndx), :)));
+end
+
 end
