e45ea3f53aa9f72ca6bdadb9f69473effe8e9cc8
Alex Ecker <aecker@cns.bcm.edu>
2013-10-28 19:15:15 -0500

    Added control analysis to Fig. 4

--- paper/fig4_ve.m
+++ paper/fig4_ve.m
@@ -23,6 +23,8 @@ keys.zscore = false;
 keys.control = 0;
 keys.int_bins = 5;
 keys = genKey(keys, 'state', flipud(unique(fetchn(nc.Anesthesia, 'state'))));
+keys(3) = keys(2);
+keys(3).control = 1;   % control analysis for anesthetized data (first 500 ms only)
 
 fig = Figure(4, 'size', [100 100]);
 M = 2; N = 2;
@@ -32,6 +34,7 @@ scax = [-1 8 -0.5 1];
 bax = [frbins([1 end]) 0 0.4];
 intbins = [1 2 5 10 20];
 ints = pro(nc.Integers, 'x -> int_bins') & sprintf('int_bins IN (0%s)', sprintf(', %d', intbins));
+linestyle = {'.-', '.-', '.:'};
 
 for i = 1 : n
 
@@ -39,21 +42,23 @@ for i = 1 : n
     [ve, fr] = fetchn(rel & keys(i), 've_test', 'mean_rate_cond');
 
     % scatter plots: variance explained vs. firing rate
-    subplot(M, N, i)
-    plot(log2(fr), ve, '.', 'color', colors(keys(i).state), 'markersize', 1)
-    axis square
-    axis(scax)
-    set(gca, 'xticklabel', 2 .^ get(gca, 'xtick'))
-    xlabel('Firing raten (spikes/s)')
-    if i == 1
-        ylabel('Variance explained')
+    if i < 3
+        subplot(M, N, i)
+        plot(log2(fr), ve, '.', 'color', colors(keys(i).state), 'markersize', 1)
+        axis square
+        axis(scax)
+        set(gca, 'xticklabel', 2 .^ get(gca, 'xtick'))
+        xlabel('Firing raten (spikes/s)')
+        if i == 1
+            ylabel('Variance explained')
+        end
     end
     
     % binned average VE as a function of firing rate
     subplot(M, N, 3)
     hold on
     [m, se, frbinsc] = makeBinned(log2(fr), ve, frbins, @mean, @(x) std(x) / sqrt(numel(x)), 'include');
-    errorbar(frbinsc, m, se, '.-', 'color', colors(keys(i).state))
+    errorbar(frbinsc, m, se, linestyle{i}, 'color', colors(keys(i).state))
     axis square
     axis(bax)
     set(gca, 'xticklabel', 2 .^ get(gca, 'xtick'))
@@ -65,7 +70,7 @@ for i = 1 : n
     hold on
     rel = nc.AnalysisStims * nc.GpfaParams * nc.GpfaModelSet * nc.GpfaVE;
     [t, v, se] = fetchn(ints, rel & rmfield(keys(i), 'int_bins'), 'bin_size * int_bins -> t', 'AVG(ve_test) -> ve', 'STD(ve_test) / SQRT(COUNT(1)) -> se');
-    errorbar(t, v, se, '.-', 'color', colors(keys(i).state))
+    errorbar(t, v, se, linestyle{i}, 'color', colors(keys(i).state))
     set(gca, 'xscale', 'log', 'xlim', intbins([1 end]) * keys(1).bin_size, ...
         'xtick', intbins * keys(1).bin_size, 'xminortick', 'off', 'ylim', [0 0.15001])
     xlabel('Integration window (ms)')
