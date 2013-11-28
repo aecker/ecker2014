b2143d4af8aac1614e60a0a23875af6dcc5dd562
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-11-28 16:50:52 +0100

    Added one-trial shift predictor to CCG

--- +nc/CrossCorr.m
+++ +nc/CrossCorr.m
@@ -4,9 +4,11 @@ nc.CrossCorr (computed) # Cross-correlogram
 -> nc.CrossCorrSet
 -> nc.UnitPairs
 ---
-ccg         : mediumblob    # cross-correlogram
-r_ccg       : mediumblob    # r_ccg normalized by variance
-r_ccg_bair  : mediumblob    # r_ccg with Bair's method (integrate var)
+ccg                 : mediumblob    # cross-correlogram
+r_ccg               : mediumblob    # r_ccg normalized by variance
+r_ccg_bair          : mediumblob    # r_ccg with Bair's method (integrate var)
+r_ccg_shift         : mediumblob    # r_ccg using shift predictor
+r_ccg_shift_bair    : mediumblob    # r_ccg using shift predictor + Bair
 %}
 
 classdef CrossCorr < dj.Relvar
