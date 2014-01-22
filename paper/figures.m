dd7ee1108e869b2bc1a8cf0fade8472d8161db71
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-01-22 09:07:51 +0100

    Renamed files for reproducing figures to reflect figure numbers in the paper

--- paper/figures.m
+++ paper/figures.m
@@ -1,5 +1,12 @@
-% Figures for ther paper
-% AE 2013-08-28
+% Code for reproducing the figures in
+%   Ecker et al. (2014): State dependence of noise correlations in macaque
+%   primary visual cortex. Neuron.
+%
+% For more information consult the readme in this Git repository or
+% http://toliaslab.org/publications/ecker-et-al-2014/
+%
+% AE 2014-01-22
+
 
 % Fig. 1: Rasters by neuron and tuning curves
 fig1_data
@@ -7,17 +14,38 @@ fig1_data
 % Fig. 2: Fano factors and noise correlations
 fig2_fano_nc
 
-% Fig. 3: Illustration of GPFA model and rasters by trial
+% Fig. 3: Rasters by trial, variance explained
 fig3_rasters
+fig3_ve
 
-% Fig. 4: Variance explained
-fig4_ve
+% Fig. 4: Model parameters: weights and timescale
+fig4_params
 
-% Fig. 5: Model parameters: weights and timescale
-fig5_params
+% Fig. 5: Residual noise correlations
+fig5_resid
 
-% Fig. 6: Residual noise correlations
-fig6_resid
+% Fig. 6: Model of unobserved common gain modulation
+fig6_model
 
-% Fig. 7: LFP as predictor of network state
-fig7_lfp
+% Fig. 7: Analysis of spontaneous activity
+fig7_spont
+
+% Fig. 8: LFP as predictor of network state
+fig8_lfp
+
+% Fig. 9: Changes in depth of anesthesia within recording sessions
+fig9_anesth_depth
+
+
+% Supplementary Figures
+
+% Fig. S1: Failure of CCG normalization suggested by Bair et al. 2001
+figS1_xcorr_fail
+
+% Fig. S2: Simulation showing different estimates of timescale depending on
+% method of normalization
+figS2_xcorr_sim
+
+% Fig. S3: Timescale of correlations in our data, measured using different
+% normalizations
+figS3_xcorr
