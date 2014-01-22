be7568e58175112f51c3b6cf97673a61e81c4b08
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-01-22 08:36:41 +0100

    Added average CCG to Suppl. Fig. 3

--- paper/suppl_fig_xcorr.m
+++ paper/suppl_fig_xcorr.m
@@ -9,31 +9,43 @@ function suppl_fig_xcorr
 key.project_name = 'NoiseCorrAnesthesia';
 key.sort_method_num = 5;
 key.spike_count_start = 30;
+key.spike_count_end = 2030;
 key.max_instability = 0.1;
 key.min_trials = 20;
 key.min_cells = 10;
 key.max_contam = 0.1;
 key.state = 'anesthetized';
 
-fig = Figure(101, 'size', [60 60]);
+fig = Figure(101, 'size', [120 60]);
 
-[ccg, c, cb] = fetchn(nc.CrossCorr * nc.Anesthesia * nc.CleanPairs & key, 'ccg', 'r_ccg', 'r_ccg_bair');
+rel = nc.CrossCorr * nc.Anesthesia * nc.CleanPairs * nc.NoiseCorrelations;
+ccg = fetchn(rel & key & 'distance > 0', 'ccg');
 ccg = mean([ccg{:}], 2);
+ccg = (ccg + flipud(ccg)) / 2;  % make symmetric
+T = size(ccg, 1);
+t = (1 : T) - (T + 1) / 2;
+
+subplot(1, 2, 1)
+plot(t, ccg, 'k')
+set(gca, 'xlim', 500 * [-1 1], 'xtick', -500 : 250 : 500, 'ylim', [0 1] * 1e-3)
+xlabel('Time lag (ms)')
+ylabel('Cross-correlation (coincidences/spike)')
+
+[c, cb] = fetchn(nc.CrossCorr * nc.Anesthesia * nc.CleanPairs & key, 'r_ccg', 'r_ccg_bair');
 c = [c{:}];
 cb = [cb{:}];
 ndx = ~any(isnan(cb) | imag(cb) | isnan(c), 1);
 C = mean(c(:, ndx), 2);
 Cb = mean(cb(:, ndx), 2);
-T = size(ccg, 1);
-[~, order] = sort(abs((1 : T) - (T + 1) / 2));
-Ccg = cumsum(ccg(order));
 t = 1 : numel(C);
-plot(t, Cb, 'r', t, C, 'k', t, Ccg(1 : 2 : end) / Ccg(end) * C(end), ':k')
+
+subplot(1, 2, 2)
+plot(t, Cb, 'r', t, C, 'k')
 set(gca, 'xscale', 'log', 'xlim', [1 2000], 'ylim', [0 0.1], 'xtick', ...
     [1 10 100 1000], 'xticklabel', [1 10 100 1000])
 xlabel('Integration time (ms)')
-ylabel('Noise correlation')
-legend({'Bair''s method', 'Normalized by total variance', 'Unnormalized and rescaled'})
+ylabel('Cumulative correlation')
+legend({'Bair''s method', 'Normalized by total variance'})
 
 fig.cleanup()
 
