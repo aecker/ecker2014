15bbdbb17219c481533f0477ae17ed9c72872a04
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2014-03-24 17:33:37 +0100

    Added path info of dependencies to startup script

--- startup.m
+++ startup.m
@@ -9,3 +9,12 @@ for i = 1:numel(d)
     addpath(fullfile(base, d(i).name))
 end
 addpath(base)
+
+% Dependencies (assumed to be in same base directory as ecker2014
+% repository and in folders with their default names)
+ndx = find(base == filesep, 1, 'last');
+base = base(1 : ndx - 1);
+addpath(fullfile(base, 'datajoint-matlab'))
+addpath(fullfile(base, 'sessions', 'schemas'))
+run(fullfile(base, 'mym', 'mymSetup'))
+run(fullfile(base, 'gpfa', 'startup'))
